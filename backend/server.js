781dbc6dcfb8a80ed69f6ee42a32465d34783d5b
salad24 <veggiemagic1124@gmail.com>
2024-09-21 01:20:26 -0400

    changed cors policy

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,28 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const cors = require('cors');
 const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-// Serve static files from the root directory
-app.use(express.static(path.join(__dirname, '../'))); // Adjust as needed
+// Enable CORS for all requests
+app.use(cors());
 
-// Serve index.html on root access
+// Serve static files
+app.use(express.static(path.join(__dirname, '../')));
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../index.html')); // Adjust to the root file
+    res.sendFile(path.join(__dirname, '../index.html'));
 });
 
 io.on('connection', (socket) => {
     console.log('A user connected');
-    
+
     socket.on('operation', (data) => {
         io.emit('update', data);
-    });
-
-    socket.on('test', (message) => {
-        socket.emit('testResponse', 'Test received!');
     });
 
     socket.on('disconnect', () => {
